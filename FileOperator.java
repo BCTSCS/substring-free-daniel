dea2e554bc84983c69bff36391346b7493933cee
sebas0505r <sebria26@bergen.org>
2025-12-09 14:10:16 -0500

    FileOperator.java

--- FileOperator.java
+++ FileOperator.java
@@ -4,9 +4,6 @@ import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-// public final class FileOperator{
-//     private static File myFile;
-//     private static Scanner fileReader;
 
 //     public static ArrayList<String> getStringData(String filename) {
 //         ArrayList<String> list = new ArrayList<>();
@@ -24,16 +21,24 @@ import java.util.ArrayList;
 //         //         fileReader.close();
 //         //     }
 //         System.out.println("continue");
-//         }
-//         return list;
-//     }
 // }
-
 public final class FileOperator{
     private static File myFile;
     private static Scanner fileReader;
 
     public static void createFile(String filename) {
+        try{
+            myFile = new File(filename);
+            fileReader = new Scanner(myFile);
+
+            while(fileReader.hasNextLine()){
+                list.add(fileReader.nextLine());
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("Error");
+        }finally{
+             System.out.println("continue");
+}
         public ArrayList<String> getStringData(String filename) {
             createFile(filename);
             ArrayList<String> list = new ArrayList<>();
@@ -41,9 +46,7 @@ public final class FileOperator{
             while(fileReader.hasNextLine()){
                 list.add(fileReader.nextLine());
             }
-        //     if (fileReader != null) {
-        //         fileReader.close();
-        //     }
         return list;
     }
+
 }
